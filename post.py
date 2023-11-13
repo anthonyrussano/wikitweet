d80b591885f2b8c49d88135d28b906150a568f7d
Anthony Russano <me@anthonyrussano.com>
2023-11-13 15:41:51 -0800

    fixed

--- post.py
+++ post.py
@@ -2,56 +2,17 @@ from requests_oauthlib import OAuth1Session
 import os
 import json
 import vars
+import random
+import markdown_it
 
-# In your terminal please set your environment variables by running the following lines of code.
-# export 'CONSUMER_KEY'='<your_consumer_key>'
-# export 'CONSUMER_SECRET'='<your_consumer_secret>'
+consumer_key = vars.OAUTH_CONSUMER_KEY
+consumer_secret = vars.OAUTH_CONSUMER_SECRET
 
-# consumer_key = os.environ.get("CONSUMER_KEY")
-# consumer_secret = os.environ.get("CONSUMER_SECRET")
-
-consumer_key = vars.CONSUMER_KEY
-consumer_secret = vars.CONSUMER_SECRET
-
-# Be sure to add replace the text of the with the text you wish to Tweet. You can also add parameters to post polls, quote Tweets, Tweet with reply settings, and Tweet to Super Followers in addition to other features.
-payload = {"text": "Hello world!"}
-
-# Get request token
-request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
 oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
 
-try:
-    fetch_response = oauth.fetch_request_token(request_token_url)
-except ValueError:
-    print(
-        "There may have been an issue with the consumer_key or consumer_secret you entered."
-    )
+access_token = vars.OAUTH_ACCESS_TOKEN
+access_token_secret = vars.OAUTH_ACCESS_TOKEN_SECRET
 
-resource_owner_key = fetch_response.get("oauth_token")
-resource_owner_secret = fetch_response.get("oauth_token_secret")
-print("Got OAuth token: %s" % resource_owner_key)
-
-# Get authorization
-base_authorization_url = "https://api.twitter.com/oauth/authorize"
-authorization_url = oauth.authorization_url(base_authorization_url)
-print("Please go here and authorize: %s" % authorization_url)
-verifier = input("Paste the PIN here: ")
-
-# Get the access token
-access_token_url = "https://api.twitter.com/oauth/access_token"
-oauth = OAuth1Session(
-    consumer_key,
-    client_secret=consumer_secret,
-    resource_owner_key=resource_owner_key,
-    resource_owner_secret=resource_owner_secret,
-    verifier=verifier,
-)
-oauth_tokens = oauth.fetch_access_token(access_token_url)
-
-access_token = oauth_tokens["oauth_token"]
-access_token_secret = oauth_tokens["oauth_token_secret"]
-
-# Make the request
 oauth = OAuth1Session(
     consumer_key,
     client_secret=consumer_secret,
@@ -59,7 +20,46 @@ oauth = OAuth1Session(
     resource_owner_secret=access_token_secret,
 )
 
-# Making the request
+
+def extract_sentences_from_markdown(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        markdown_text = file.read()
+
+    # Initialize the Markdown parser
+    md = markdown_it.MarkdownIt()
+
+    # Parse the Markdown file and extract text
+    parsed_text = md.render(markdown_text)
+
+    # Split the text into sentences
+    sentences = parsed_text.split(".")
+
+    # Remove empty sentences and HTML-encoded entities
+    sentences = [
+        sentence.strip().replace("&lt;", "<").replace("&gt;", ">")
+        for sentence in sentences
+        if sentence.strip()
+    ]
+
+    return sentences
+
+
+# Specify the path to your Markdown file
+markdown_file = "output.md"
+
+# Extract sentences from the Markdown file
+sentences = extract_sentences_from_markdown(markdown_file)
+
+# Choose a random sentence to tweet
+selected_sentence = random.choice(sentences)
+
+max_tweet_length = 280  # Twitter's previous character limit
+if len(selected_sentence) > max_tweet_length:
+    selected_sentence = selected_sentence[:max_tweet_length]
+
+payload = {"text": selected_sentence}
+
+
 response = oauth.post(
     "https://api.twitter.com/2/tweets",
     json=payload,
@@ -72,6 +72,5 @@ if response.status_code != 201:
 
 print("Response code: {}".format(response.status_code))
 
-# Saving the response as JSON
 json_response = response.json()
 print(json.dumps(json_response, indent=4, sort_keys=True))
